7f9353710717cd3ad0624b153c69d4fc7c989d25
naveen-pete <naveen.shenoy.blr@gmail.com>
2019-11-11 18:02:31 +0530

    commit code at the end of day 1

---
+++ apps/day-1/ts-examples/app.js
@@ -0,0 +1,8 @@
+var customerId = 10;
+var customerName = 'Hari';
+var customerAge = 25;
+var customerSpecial = true;
+// let sentence: string = 'Hello, my name is ' + customerName + '. My age is ' + customerAge ;
+var sentence = "Hello, my name is " + customerName + ". \n\nMy age is " + customerAge;
+// let customerName = 'Hari';
+console.log('sentence:', sentence);
---
+++ apps/day-1/ts-examples/app.ts
@@ -0,0 +1,13 @@
+let customerId: number = 10;
+let customerName: string = 'Hari';
+let customerAge: number = 25;
+let customerSpecial: boolean = true;
+
+// let sentence: string = 'Hello, my name is ' + customerName + '. My age is ' + customerAge ;
+let sentence: string = `Hello, my name is ${customerName}. 
+
+My age is ${customerAge}`;
+
+// let customerName = 'Hari';
+
+console.log('sentence:', sentence);
